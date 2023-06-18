255e250789b4b7ebebae0d1493909dbee320f9be
Ahmed Jaber <117093494+Codingaway20@users.noreply.github.com>
2023-06-18 20:53:30 +0300

    Delete Contiki/platform/xm1000 directory

--- Contiki/platform/xm1000/apps/README.txt
+++
@@ -1,4 +0,0 @@
-I tested Blink app, it seems to work, type the following:
-
-make TARGET=xm1000 blink.upload
-
--- Contiki/platform/xm1000/apps/sensors/install.sh
+++
@@ -1,8 +0,0 @@
-make TARGET=xm1000 savetarget
-make sensorReading
-sudo make sensorReading.upload
-
-echo "running your application"
-#sudo ../../tools/sky/serialdump-linux -b115200 /dev/ttyUSB0
-sudo make login
-
